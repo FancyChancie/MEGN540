e5f09982cf335fbae89e47b74968e80ef84d3cce
Chance <chance1616@gmail.com>
2022-03-02 18:43:56 -0700

    Battery_Monitor.c written

--- c_lib/Battery_Monitor.c
+++ c_lib/Battery_Monitor.c
@@ -1,15 +1,32 @@
 #include "Battery_Monitor.h"
 
-static const float BITS_TO_BATTERY_VOLTS = 0;
+/*
+NOT SURE WHAT TO DO HERE!!!!
+*/
+static const float BITS_TO_BATTERY_VOLTS = 1023;
 
 /**
  * Function Battery_Monitor_Init initializes the Battery Monitor to record the current battery voltages.
  */
 void Battery_Monitor_Init()
 {
+    // VBAT pin PF6/ADC6 (aka A1)
+    // Enable ADC (Sec. 24.9.2)
+    ADCSRA |= (1 << ADEN);
 
-	// *** MEGN540 LAB3 YOUR CODE HERE ***
+    // Set prescalor to 128 (Sec 24.9.2)
+    // Need within 50kHz-200kHz range: 16MHz/128 = 125kHz
+    ADCSRA |= (1 << ADPS2);
+    ADCSRA |= (1 << ADPS1);
+    ADCSRA |= (1 << ADPS0);
 
+    // Set internal 2.56V reference with external capacitor on AREF pin (Sec.24.9.1)
+    ADMUX |= (1 << REFS1);
+    ADMUX |= (1 << REFS0);
+
+    // Enable ADC6 as input (Sec.24.9.1)
+    ADUMUX |= (1 << MUX2);
+    ADUMUX |= (1 << MUX1);
 }
 
 /**
@@ -17,11 +34,24 @@ void Battery_Monitor_Init()
  */
 float Battery_Voltage()
 {
-    // A Union to assist with reading the LSB and MSB in the  16 bit register
+    // A Union to assist with reading the LSB and MSB in the 16 bit register
     union { struct {uint8_t LSB; uint8_t MSB; } split; uint16_t value;} data;
+    // Store interrupt settings (this is like ATOMIC_BLOCK(ATOMIC_FORCEON)
+    SREG_copy = SREG;
+        // Disable global interrupts
+        cli();
+        // Start ADC conversion (resets itself to 0 after conversion complete) (Sec. 24.9.2)
+        ADCSRA |= (1 << ADSC);
+        while(ADSC){
+            // Save ADC Low byte into union stuct (Sec. 24.9.3)
+            data.split.LSB = ADCL;
+            // Save ADC high byte into union struct
+            data.split.MSB = ADCH;
+        }
+        // Enable global interrupts
+        sei();
+    // Restore interrupt settings
+    SREG = SREG_copy;
 
-    // *** MEGN540 LAB3 YOUR CODE HERE ***
-
-    return data.value * BITS_TO_BATTERY_VOLTS;
-
+    return (float) data.value * BITS_TO_BATTERY_VOLTS;
 }
