b0eb69a573d4a991992aac9bfd90664d6c2ce139
Chance <chance1616@gmail.com>
2022-04-08 16:46:06 -0600

    Made some minor changes

--- c_lib/Controller.c
+++ c_lib/Controller.c
@@ -42,11 +42,12 @@
 /**
  * Function Initialize_Controller setsup the z-transform based controller for the system.
  */
-void Controller_Init(Controller_t* p_cont, float kp, float* num, float* den, uint8_t order, float update_period)
+void Controller_Init( Controller_t* p_cont, float kp, float* num, float* den, uint8_t order, float update_period )
 {
     p_cont->kp = kp;
     p_cont->update_period = update_period;
-    p_cont->controller = Filter_Init(&p_cont->controller, num, den, order); // Initalize control filter
+    //p_cont->controller = Filter_Init(&p_cont->controller, num, den, order); // Initalize control filter
+    Filter_Init(&p_cont->controller,num,den,order); // Initalize control filter
 }
 
 /**
@@ -64,7 +65,7 @@ void Controller_Set_Target_Velocity( Controller_t* p_cont, float vel )
  */
 void Controller_Set_Target_Position( Controller_t* p_cont, float vel )
 {
-    p_cont->target_vel = 0;
+    p_cont->target_vel = 0.0;
     p_cont->target_pos = vel * p_cont->update_period;
 }
 
@@ -91,23 +92,23 @@ float Controller_Update( Controller_t* p_cont, float measurement, float dt )
  */
 float Controller_Last( Controller_t* p_cont)
 {
-    return last_control_command;
+    return Filter_Last_Output(&p_cont->controller);
 }
 
 /**
  * Function Controller_SettTo sets the Filter's input and output lists
  * to match the measurement so it starts with zero error.
  */
-void Controller_SetTo(Controller_t* p_cont, float measurement )
+void Controller_SetTo( Controller_t* p_cont, float measurement )
 {
-    Filter_SetTo(p_cont, measurement);
+    Filter_SetTo(&p_cont->controller, measurement);
 }
 
 /**
  * Function Controller_ShiftBy shifts the Filter's input and output lists
  * by the desired amount. This is helpful when dealing with wrapping.
  */
-void Controller_ShiftBy(Controller_t* p_cont, float measurement )
+void Controller_ShiftBy( Controller_t* p_cont, float measurement )
 {
-    Filter_ShiftBy(p_cont, measurement);
+    Filter_ShiftBy(&p_cont->controller, measurement);
 }
